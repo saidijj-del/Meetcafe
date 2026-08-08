51de0211a29c18d7792185f3efd221177c1b01d4
saidijj-del <saidijj@gmail.com>
2026-08-08 12:29:31 +0100

    Create app.dart

---
+++ lib/app.dart
@@ -0,0 +1,35 @@
+import "package:flutter/material.dart";
+import "package:flutter_localizations/flutter_localizations.dart";
+import "package:provider/provider.dart";
+
+import "core/router.dart";
+import "core/theme.dart";
+import "l10n/translations.dart";
+
+class MeetCafeApp extends StatelessWidget {
+  const MeetCafeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final lang = context.watch<LanguageProvider>();
+
+    return MaterialApp.router(
+      title: "MeetCafe",
+      debugShowCheckedModeBanner: false,
+      theme: MeetCafeTheme.light,
+      routerConfig: MeetCafeRouter.config,
+      locale: Locale(lang.lang),
+      supportedLocales: const [
+        Locale("en"), Locale("ar"), Locale("es"), Locale("fr"),
+        Locale("zh"), Locale("hi"), Locale("pt"), Locale("ru"),
+        Locale("de"), Locale("ja"),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+    );
+  }
+}
+
