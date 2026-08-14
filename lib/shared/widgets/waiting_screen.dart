f3c67d4480c8217d0068412fad7d59d2763357ec
saidijj-del <saidijj@gmail.com>
2026-08-14 03:06:17 +0100

    Implement loading animation in WaitingScreen
    Added a loading animation with dots to the WaitingScreen widget.

--- lib/shared/widgets/waiting_screen.dart
+++ lib/shared/widgets/waiting_screen.dart
@@ -1,4 +1,3 @@
-
 import "package:flutter/material.dart";
 
 class WaitingScreen extends StatefulWidget {
@@ -102,6 +101,81 @@ class _WaitingScreenState extends State<WaitingScreen>
             Text(
               widget.title,
               textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF1C1917),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              widget.desc,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Color(0xFF78716C),
+                height: 1.4,
+              ),
+            ),
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(3, (i) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: _Dot(delay: i * 300),
+                );
+              }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Dot extends StatefulWidget {
+  final int delay;
+  const _Dot({required this.delay});
+
+  @override
+  State<_Dot> createState() => _DotState();
+}
+
+class _DotState extends State<_Dot> with SingleTickerProviderStateMixin {
+  late final AnimationController _c = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 600),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: widget.delay), () {
+      if (mounted) _c.repeat(reverse: true);
+    });
+  }
+
+  @override
+  void dispose() {
+    _c.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: Tween(begin: 0.3, end: 1.0).animate(_c),
+      child: Container(
+        width: 8,
+        height: 8,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Color(0xFFD97706),
+        ),
+      ),
+    );
+  }
+}
               style: const TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
