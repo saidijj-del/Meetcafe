6ad854a7c3c2b5d0538a5b933afea019f8a35517
saidijj-del <saidijj@gmail.com>
2026-08-14 18:32:15 +0100

    Update import statements for MeetCafe packages

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,9 @@
 import "package:flutter/material.dart";
-import "package:flutter_localizations/flutter_localizations.dart";
 import "package:provider/provider.dart";
 
-import "core/router.dart";
-import "core/theme.dart";
-import "l10n/translations.dart";
+import "package:meetcafe/core/router.dart";
+import "package:meetcafe/core/theme.dart";
+import "package:meetcafe/l10n/translations.dart";
 
 class MeetCafeApp extends StatelessWidget {
   const MeetCafeApp({super.key});
@@ -32,4 +31,3 @@ class MeetCafeApp extends StatelessWidget {
     );
   }
 }
-
